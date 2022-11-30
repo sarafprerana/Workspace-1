fbfe3d39cd7dbbe93dee17c305472464dd0f1917
myapp <program@gmail.com>
2022-11-30 16:09:34 +0530

    new

--- bootstrap/fun1.js
+++ bootstrap/fun1.js
@@ -1,4 +1,17 @@
-function register()
-{
-    alert("jfhkjfd");
-}
+
+const formDetails = document.getElementById("details");
+formDetails.addEventListener("submit", async (ev) => {
+   ev.preventDefault();
+   var fName = document.getElementById("fname").value;
+   var lName = document.getElementById("lname").value;
+   var h =document.getElementById("hobbies").value;
+   var h1=document.getElementById("hobbies1").value;
+   var male=document.getElementById("gm").value;
+   var female =document.getElementById("gf").value;
+   document.write("First Name=" + fName);
+   document.write("Last Name=" + lName);
+   document.write("Hobbies="+h);
+   document.write("Hobbies="+h1);
+   document.write("Gender="+male);
+   document.write("Gender="+female);
+})
