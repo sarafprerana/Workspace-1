1d43e741ef55bae94550ae68c01ae12bb1c0d0d5
myapp <program@gmail.com>
2022-12-03 11:58:14 +0530

    updated

--- bootstrap/fun1.js
+++ bootstrap/fun1.js
@@ -1,20 +1,29 @@
 
-const formDetails = document.getElementById("details");
-formDetails.addEventListener("Submit", async (ev) => {
-   ev.preventDefault();
-   var fName = document.getElementById("fname").value;
-   var lName = document.getElementById("lname").value;
-   var h =document.getElementById("hobbies").value;
-   var h1=document.getElementById("hobbies1").value;
-   var male=document.getElementById("gm").value;
-   var female =document.getElementById("gf").value;
-   document.write("First Name=" + fName);
-   document.write("Last Name=" + lName);
-   document.write("Hobbies="+h);
-   document.write("Hobbies="+h1);
-   document.write("Gender="+male);
-   document.write("Gender="+female);
+ const formDetails = document.getElementById("details");
+ formDetails.addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+    var fName = document.getElementById("fname").value;
+    var lName = document.getElementById("lname").value;
+    document.write("First Name=" + fName);
+    document.write("Last Name=" + lName);
+     
+    var gm=document.getElementById("gm").value;
+    document.write("gender"+gm);
    
-})
+ })
+ /*const formDetails = document.getElementById("details");
+ formDetails.addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+      
+   var a;
+   if(a=MSC)
+   {
+     document.write(a);
+   }
+   else{
+     document.write("Not available");
+   }
+  });*/
+  
 
-
+ 
