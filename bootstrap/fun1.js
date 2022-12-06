9612d1346eb1b5d9197530896ec108ad7b21d926
myapp <sarafprerana1@gmail.com>
2022-12-06 15:02:14 +0530

    node pract

--- bootstrap/fun1.js
+++ bootstrap/fun1.js
@@ -7,15 +7,16 @@
     document.write("First Name=" + fName);
     document.write("Last Name=" + lName);
      
-    var gm=document.getElementById("gm").value;
-    document.write("gender"+gm);
+    var date=document.getElementById('dob').value;
+    document.write("Date="+date);
    
  })
  /*const formDetails = document.getElementById("details");
  formDetails.addEventListener("submit", async (ev) => {
     ev.preventDefault();
       
-   var a;
+   var a=MSC;
+   var b=BSc
    if(a=MSC)
    {
      document.write(a);
@@ -23,7 +24,20 @@
    else{
      document.write("Not available");
    }
-  });*/
+  });
   
 
- 
+ var form= document.getElementById('form')
+
+ form.addEventListener('submit',function(event){
+
+  event.preventDefault() //autosubmitting
+
+  var fName=document.getElementById('fname').value;
+
+  document.write("First Name="+fName);
+
+  var lName=document.getElementById('lname').value;
+
+  document.write("Last Name="+lName);
+ })*/
---
+++ node/index.js
@@ -0,0 +1,30 @@
+console.log("Hiiiii..........!")
+console.log("Hello..........")
+
+// var moment=require('moment')
+// console.log(moment().format());
+
+// import moment from 'moment';//ES6 syntax
+//  moment().format();
+
+ //import moment from 'moment-timezone/builds/moment-timezone-with-data-2012-2022';
+
+//  var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(8080);
+
+ var moment=require('moment')
+// console.log(moment().weekday(-10));
+
+//moment().dayOfYear(Number);
+//console.log(moment().dayOfYear(115)); // Number
+
+// console.log(moment().day());
+
+console.log(moment().millisecond());
+
+// console.log(moment().second(150));
+
+// console.log(moment().minute(10));
