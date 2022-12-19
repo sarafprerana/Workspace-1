656d36989bc074154c0ba96881ea1b2dc8c8353d
myapp <sarafprerana1@gmail.com>
2022-12-19 15:52:42 +0530

    node update

--- node/server.js
+++ node/server.js
@@ -1,56 +1,110 @@
-var _ = require('lodash');
-var array = _.groupBy([
- array1= {    
-  "employeeName": "John",    
-  "experience": 12,    
-  "technology": "JAVA"    
-}, array2={    
-  "employeeName": "Charles",    
-  "experience": 9,    
-  "technology": "C#"    
-}, array3={    
-  "employeeName": "Jo",    
-  "experience": 3,    
-  "technology": "JAVA"    
-}, array4={    
-  "employeeName": "Daine",    
-  "experience": 7,    
-  "technology": "Sql Server"    
-}, array5={    
-  "employeeName": "Zain",    
-  "experience": 6,    
-  "technology": "C#"},],"technology");  
+//var _ = require('lodash');
+/*var array =[
+ {    
+  "EmployeeName": "Prerana",    
+  "Experience": 12,    
+  "Technology": "JAVA"    
+}, {    
+  "EmployeeName": "Shrikant",    
+  "Experience": 9,    
+  "Technology": "C#"    
+}, {    
+  "EmployeeName": "Ovi",    
+  "Experience": 3,    
+  "Technology": "JAVA"    
+}, {    
+  "EmployeeName": "Aarohi",    
+  "Experience": 7,    
+  "Technology": "Sql Server"    
+}, {    
+  "EmployeeName": "Yash",    
+  "Experience": 6,    
+  "Technology": "C#"}];  
 console.log(array);  
 //Comparer Function    
-/*function GetSortOrder(prop) {    
+function GetSortOrder(prop) {    
   return function(a, b) {    
-      if (a[prop] < b[prop]) {    
+      if (a[prop] > b[prop]) {    
           return 1;    
-      } else if (a[prop] >b[prop]) {    
+      } else if (a[prop] <b[prop]) {    
           return -1;    
       } 
       else   
       return 0;    
   }    
 }    
-  
  array.sort(GetSortOrder("EmployeeName")); //Pass the attribute to be sorted on    
  console.log("---------Sorted Employee Names : ----------");    
  for (var item in array) {    
    console.log( array[item].EmployeeName);    
  }    
-  
  array.sort(GetSortOrder("Technology")); //Pass the attribute to be sorted on    
  console.log("------------Sorted Technology Names :------------ ");    
  for (var item in array) {    
    console.log(array[item].Technology);    
  } 
-
  array.sort(GetSortOrder("Experience")); //Pass the attribute to be sorted on    
  console.log("--------------Sorted Experience :-----------");    
  for (var item in array) {    
    console.log(array[item].Experience);    
- }   */
+ }   
+*/
+
+var array = [{    
+  "EmployeeName": "John",    
+  "Experience": 12,    
+  "Technology": "SharePoint"    
+}, {    
+  "EmployeeName": "Charles",    
+  "Experience": 9,    
+  "Technology": "ASP.NET"    
+}, {    
+  "EmployeeName": "Jo",    
+  "Experience": 3,    
+  "Technology": "JAVA"    
+}, {    
+  "EmployeeName": "Daine",    
+  "Experience": 7,    
+  "Technology": "Sql Server"    
+}, {    
+  "EmployeeName": "Zain",    
+  "Experience": 6,    
+  "Technology": "C#"    
+}];    
+//Comparer Function    
+function GetSortOrder(prop) {    
+  return function(a, b) {    
+      if (a[prop] < b[prop]) {    
+          return 1;    
+      } else if (a[prop] > b[prop]) {    
+          return -1;    
+      }
+      else  
+      return 0;    
+  }    
+} 
+ array.sort(GetSortOrder("EmployeeName")); //Pass the attribute to be sorted on    
+ console.log("---------Sorted Employee Names : ----------");    
+ for (var item in array) {    
+ console.log( array[item].EmployeeName);    
+ }    
+ array.sort(GetSortOrder("Technology")); //Pass the attribute to be sorted on    
+ console.log("------------Sorted Technology Names :------------ ");    
+ for (var item in array) {    
+ console.log(array[item].Technology);    
+ }
+ array.sort(GetSortOrder("Experience")); //Pass the attribute to be sorted on    
+ console.log("--------------Sorted Experience :-----------");    
+ for (var item in array) {    
+ console.log(array[item].Experience);    
+ }  
+
+
+
+
+
+
+
 //  var _ = require('lodash');
 //  console.log("------------GroupeBy Technology------------")
 //  result = _.groupBy(['Java', 'ASP.NET', 'Sql server','C#','C#'], 'technology');
